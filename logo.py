a8a7e6e527c1030fd1dc8d68290522b8c3027a06
Destroyer901 <76010338+Destroyed901@users.noreply.github.com>
2022-10-21 21:49:18 +0300

    Add files via upload

---
+++ logo.py
@@ -0,0 +1,15 @@
+from colorama import Fore
+from colorama import Fore, Back, Style
+from termcolor import colored
+banner = colored("""
+______          _
+|  _  \        | |
+| | | |___  ___| |_ _ __ ___  _   _  ___ _ __
+| | | / _ \/ __| __| '__/ _ \| | | |/ _ \ '__|
+| |/ /  __/\__ \ |_| | | (_) | |_| |  __/ |
+|___/ \___||___/\__|_|  \___/ \__, |\___|_|
+                               __/ |
+                              |___/
+                    channel: https://t.me/eniac_tg
+		
+""", "green")
